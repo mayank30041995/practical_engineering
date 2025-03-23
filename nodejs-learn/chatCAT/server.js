d833cc6cd53a5308d9b761f65bc1cdd24dbdf93b
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-03-23 15:02:09 +0530

    ejs view engine for view

--- nodejs-learn/chatCAT/server.js
+++ nodejs-learn/chatCAT/server.js
@@ -3,11 +3,13 @@ const express = require('express')
 const app = express()
 
 app.set('port', process.env.PORT || 3000)
-
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
 app.get('/', (req, res, next) => {
   //   res.send('<h1>Hello Express</h1>')
-  console.log(__dirname)
-  res.sendFile(__dirname + '/views/login.htm')
+  res.render('chatroom', {
+    pageTitle: 'My Login Page',
+  })
 })
 
 app.listen(app.get('port'), () => {
