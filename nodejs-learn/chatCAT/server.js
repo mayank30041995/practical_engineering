cb82aa391db0c466f5f33cf9d89670327edaf9ff
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-03-23 14:29:27 +0530

    The view render

--- nodejs-learn/chatCAT/server.js
+++ nodejs-learn/chatCAT/server.js
@@ -4,26 +4,10 @@ const app = express()
 
 app.set('port', process.env.PORT || 3000)
 
-let helloMiddleware = (req, res, next) => {
-  req.hello = "Hello! It's me! I was wondering.... you get the idea!"
-  next()
-}
-
-// app.use(helloMiddleware)
-// app.use('/', helloMiddleware)
-app.use('/dashboard', helloMiddleware)
-
 app.get('/', (req, res, next) => {
-  res.send('<h1>Hello Express</h1>')
-  console.log(req.hello)
-})
-
-app.get('/dashboard', (req, res, next) => {
-  res.send(
-    '<h1>This is the dashboard page!<br/> MiddleWare says ' +
-      req.hello +
-      ' </h1>'
-  )
+  //   res.send('<h1>Hello Express</h1>')
+  console.log(__dirname)
+  res.sendFile(__dirname + '/views/login.htm')
 })
 
 app.listen(app.get('port'), () => {
