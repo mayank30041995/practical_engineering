28fc130ebb43d5c411acd7a817db60b00e03bbe1
Mayank Kumar Sharma <mayankece155@gmail.com>
2025-03-23 19:21:18 +0530

    routes module in better way

--- nodejs-learn/chatCAT/app/index.js
+++ nodejs-learn/chatCAT/app/index.js
@@ -1,14 +1,4 @@
 'use strict'
-const router = require('express').Router()
-
-router
-  .get('/', (req, res, next) => {
-    res.render('login')
-  })
-  .post((req, res, next) => {
-    res.render('post router')
-  })
-
 module.exports = {
-  router,
+  router: require('./routes')(),
 }
---
+++ nodejs-learn/chatCAT/app/routes/index.js
@@ -0,0 +1,44 @@
+"use strict"
+const router = require("express").Router()
+module.exports = () => {
+  let routes = {
+    "get": {
+      "/": (req, res, next) => {
+        res.render("login")
+      },
+      "/rooms": (req, res, next) => {
+        res.render("rooms")
+      },
+      "/chatroom": (req, res, next) => {
+        res.render("chatroom")
+      },
+    },
+    "post": {},
+    "NA": (req, res, next) => {
+      res.status(404).sendFile(process.cwd() + "/views/404.htm")
+    },
+  }
+
+  //Iterate through the routes object and mount the routes
+  let registerRoutes = (routes, method) => {
+    for (let key in routes) {
+      if (
+        typeof routes[key] === "object" &&
+        routes[key] !== null &&
+        !(routes[key] instanceof Array)
+      ) {
+        registerRoutes(routes[key], key)
+      } else {
+        // Register the routes
+        if (method === "get") {
+          router.get(key, routes[key])
+        } else if (method === "post") {
+          router.post(key, routes[key])
+        }
+      }
+    }
+  }
+
+  registerRoutes(routes)
+  return router
+}
